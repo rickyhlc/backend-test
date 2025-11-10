681cb64420d3c93331c34c7945baf9b302051eaf
rickyhung <rickyzero@outlook.com>
2025-11-10 18:15:50 +0800

    add listener

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const PORT = 8080;
 const wss = new WebSocketServer({ port: PORT });
 
 wss.on('connection', function connection(ws) {
+  console.log('New client connected');
+
   ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+  ws.on('open', () => {
+    console.log('Client connected');
+    ws.send(Date.now());
   });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+  // ws.on('message', function message(data, isBinary) {
+  //   wss.clients.forEach(function each(client) {
+  //     if (client.readyState === WebSocket.OPEN) {
+  //       client.send(data, { binary: isBinary });
+  //     }
+  //   });
+  // });
 });
